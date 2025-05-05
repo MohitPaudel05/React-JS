9b5db4dec6c69503431277b22f33fbcf029b5a56
MohitPaudel05 <mohitpoudel39@gmail.com>
2025-05-05 22:19:44 +0545

    new project

---
+++ mohit/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className='nav'>
+      <div className='navbar'>
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>About</a></li>
+          <li><a>Service</a></li>
+          <li><a>Contact</a></li>
+          
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
